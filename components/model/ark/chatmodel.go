131a4d10b9100a53a95bb364a51fc52e91b341a9
geyihao <geyihao@bytedance.com>
2025-12-04 20:57:28 +0800

    fix

--- components/model/ark/chatmodel.go
+++ components/model/ark/chatmodel.go
@@ -208,6 +208,13 @@ func buildChatCompletionAPIChatModel(config *ChatModelConfig) *completionAPIChat
 		arkruntime.WithRegion(region),
 		arkruntime.WithTimeout(timeout),
 	}
+	if config.UseBatchChat != nil && *config.UseBatchChat {
+		opts = []arkruntime.ConfigOption{
+			arkruntime.WithBaseUrl(baseURL),
+			arkruntime.WithRegion(region),
+		}
+	}
+
 	if config.HTTPClient != nil {
 		opts = append(opts, arkruntime.WithHTTPClient(config.HTTPClient))
 	}
